9cc23bd6a08932ec78f54550946b3e1a4f426e51
Tony Rogvall <tony@rogvall.se>
2020-08-06 13:45:52 +0200

    removed newline from git output

--- setup.py
+++ setup.py
@@ -4,13 +4,13 @@ def erlpath():
     s = os.popen("erl -noshell -eval \"io:format('~s',[code:root_dir()])\" -s erlang halt")
     p = s.read()
     s.close()
-    return p
+    return p.rstrip()
 
 def vsn():
     s = os.popen("git describe --tags --abbrev=0")
     p = s.read()
     s.close()
-    return p
+    return p.rstrip()
 
 from distutils.core import setup, Extension
 setup(name = 'varpy',
